332fd7430350ab436229ef7e5ea1e2159561512b
bahtirek <bahtirek@gmail.com>
2024-03-03 23:36:51 -0500

    change: added float menu for more menu

--- src/app/components/float-menu/float-menu.component.ts
+++ src/app/components/float-menu/float-menu.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input } from '@angular/core';
-import { FloatMenu } from '../../shared/interfaces/float-menu.interface';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-float-menu',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './float-menu.component.html',
   styleUrl: './float-menu.component.scss'
 })
 export class FloatMenuComponent {
+  modalValue: boolean = false;
+  fadeAnim: boolean = false;
+  fadeIn: boolean = false;
 
-  @Input() menuItems: FloatMenu[] = []
+  @Output() modalChange = new EventEmitter<boolean>();
+  hideContainer: boolean = true;
+
+  @Input() get modal(){
+    return this.modalValue;
+  }
+
+  set modal(val) {
+    if (val) {
+      this.hideContainer = false;
+      this.fadeAnim = true;
+      setTimeout(() => {
+        this.fadeIn = true;
+      });
+    } else {
+      this.fadeIn = false;
+      setTimeout(() => {
+        this.fadeAnim = false;
+        this.hideContainer = true;
+      }, 400);
+    }
+
+    this.modalValue = val;
+    this.modalChange.emit(this.modalValue);
+  }
+
+  closeModal(){
+    this.modal = false;
+  }
 
 }
