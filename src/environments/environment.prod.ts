6360cf2e8853fe3d1cac962f0b906982258bc6ff
bahtirek <bahtirek@gmail.com>
2024-03-03 23:35:37 -0500

    change: build warnings fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://nodejs-production-e941.up.railway.app/api',
+  socketUrl: 'https://nodejs-production-e941.up.railway.app',
 };
