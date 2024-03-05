100805fac9ef6acc977fd832810bc10a327897f7
bahtirek <bahtirek@gmail.com>
2024-03-04 21:32:48 -0500

    change: toggle menu by id

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,23 @@
 import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
-import { IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuToggle, IonThumbnail, IonIcon } from "@ionic/angular/standalone";
+import { IonHeader, IonTitle, IonToolbar, IonButtons,  IonThumbnail, IonIcon, MenuController } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { menuOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [IonButtons, IonToolbar, IonTitle, IonHeader, IonMenuToggle, IonThumbnail, IonIcon,RouterLink, RouterLink, RouterLinkActive],
+  imports: [IonButtons, IonToolbar, IonTitle, IonHeader, IonThumbnail, IonIcon,RouterLink, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   private auth = inject(AuthService);
   toggleMenuModal: boolean = false;
-  constructor() {
+  constructor(private menuCtrl: MenuController) {
     addIcons({menuOutline})
   }
 
-
-  openMenu(){
-    this.toggleMenuModal = true
-  }
-
-  closeMenu(){
-    this.toggleMenuModal = false
-  }
+  toggleSideMenu() { this.menuCtrl.toggle('mainMenu'); }
 }
