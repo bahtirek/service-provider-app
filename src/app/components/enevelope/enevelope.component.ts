6a4cce6a0fa9f58a14786472d0e5f545c660bfea
bahtirek <bahtirek@gmail.com>
2024-03-04 21:33:27 -0500

    change:  used ionic icon

--- src/app/components/enevelope/enevelope.component.ts
+++ src/app/components/enevelope/enevelope.component.ts
@@ -1,14 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chatbubbleEllipsesOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-enevelope',
   standalone: true,
-  imports: [],
+  imports: [IonIcon],
   templateUrl: './enevelope.component.html',
   styleUrl: './enevelope.component.scss'
 })
 export class EnevelopeComponent {
-  count: number = 0
+  count: number = 0;
+
+  constructor() {
+    addIcons({chatbubbleEllipsesOutline})
+  }
 
   @Input() set newMessageCount(count: number){
     if(count && count > 0) this.count = count
